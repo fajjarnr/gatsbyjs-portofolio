0a550592e453f3b584cd8d746c55714802775455
fajjarnr <zfajart@gmail.com>
2020-08-19 15:55:06 +0700

    create Hero.js

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from "react"
+// import "../css/main.css"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      {/* <Sidebar />
+      <Footer /> */}
+    </>
+  )
+}
+
+export default Layout
